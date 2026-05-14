821ea8ebd1387a1d859c7be6d296c2cae3ec2e2e
JohnTabler <131303699+JohnTabler@users.noreply.github.com>
2026-05-14 13:18:40 -0700

    Change output JSON file path to 'data/DataTest.json'

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -3,5 +3,5 @@ import csv, json
 with open('DataTest.csv', encoding='utf-8-sig') as f:
     rows = list(csv.DictReader(f))
 
-with open('DataTest.json', 'w') as f:
+with open('data/DataTest.json', 'w') as f:
     json.dump(rows, f, indent=2)
